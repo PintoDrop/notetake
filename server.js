02611e2c7f4b16f8a39828b158b6dfcf70364dd8
PintoDrop <mikebender22@gmail.com>
2023-03-22 08:50:30 -0700

    added public folder for htmls and assets

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const app = express()
+const PORT = 3001;
+const app = express();
+const path = require('path');
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true}));
 
 
 
 app.listen(PORT, () => 
-
-)
+  console.log(`Listening at https://localhost:${PORT}`)
+);
