13df0537f38ea3bcf1c75e950bd9ddd731965f39
PintoDrop <mikebender22@gmail.com>
2023-03-22 09:34:45 -0700

    added some server.js code

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 const path = require('path');
+const fs = require('fs');
+// const notes = require('./db/db.json');
 
 
 app.use(express.json());
@@ -9,7 +11,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true}));
 
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+});
+
+app.get("/notes", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"))
+});
+
+
+
 
 app.listen(PORT, () => 
-  console.log(`Listening at https://localhost:${PORT}`)
+  console.log(`Go to http://localhost:${PORT}`)
 );
