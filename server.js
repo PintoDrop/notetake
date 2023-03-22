288102d0b6bc96c85e387617571124583371a6d0
PintoDrop <mikebender22@gmail.com>
2023-03-22 11:54:54 -0700

    testing some code for saving notes

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const PORT = 3001;
 const app = express();
 const path = require('path');
 const fs = require('fs');
-// const notes = require('./db/db.json');
+const notes = require('./db/db.json');
 
+const api = require('./routes');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,6 +19,27 @@ app.get("/", (req, res) => {
 app.get("/notes", (req,res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"))
 });
+
+app.post("/api/notes", (req, res) => {
+  res.json(notes)
+})
+
+// if(err) {
+//   console.log(err)
+// }
+// console.log(notes)
+
+// to add/read notes to db.json:
+// fs.readFile("./db/db.json",)
+
+// const notes = JSON.parse(data)
+// notes.push(newNote)
+
+// fs.writeFile("./db/db.json", JSON.stringify(notes) => {
+//   ? console.log(notes);
+//   : console
+// })
+
 
 
 
