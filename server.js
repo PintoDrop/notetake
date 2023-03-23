888d4e015f3c4171fcd80b04c366a40b9bda5c01
PintoDrop <mikebender22@gmail.com>
2023-03-23 15:55:27 -0700

    fixing routes and adding read and write note code

--- server.js
+++ server.js
@@ -4,59 +4,58 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const notes = require("./db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
-// const api = require('./routes');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("./helpers/fsUtils");
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 app.post("/api/notes", (req, res) => {
-  let newNote = {
-    title: req.body.title,
-    text: req.body.text,
-  };
-  // notes.push(newNote)
-  // fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes), err => {
-  //   if(err) { console.log('There was an error!') }
-  //   res.json(newNote)
-  // })
-});
+  let { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    }
+    readAndAppend(newNote, './db/db.json');
+    res.json('New note added')
+  } else {
+    res.errored('Error when adding new note');
+  }
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json)=>{
+      const result = json.filter((note) => note.id !== id);
+      writeToFile("./db/db.json", result);
+      res.json('Note is removed');
+    })
+  })
+  
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+
 
 app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`));
 
-// app.get("/api/notes", (req, res) =>{
-//   res.json(notes)
-// })
-
-// app.get('../db/db.json', (req, res) => {
-//   res.json(notes)
-// })
-
-// if(err) {
-//   console.log(err)
-// }
-// console.log(notes)
-
-// to add/read notes to db.json:
-// fs.readFile("./db/db.json")
-
-// const notes = JSON.parse(data)
-// notes.push(newNote)
-
-// fs.writeFile("./db/db.json", JSON.stringify(notes) => {
-//   ? console.log(notes);
-//   : console.log(notes);
-// })
