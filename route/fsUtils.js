953f930961c6a6dddf93d6d71041a741b1954e01
PintoDrop <mikebender22@gmail.com>
2023-03-24 09:59:16 -0700

    touched up some code

--- route/fsUtils.js
+++ route/fsUtils.js
@@ -4,7 +4,7 @@ const readFromFile = util.promisify(fs.readFile);
 
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-  err ? console.error(err) : console.info(`\nNote written to ${destination}`)
+  err ? console.error(err) : console.info(`\nNew note written to ${destination}`)
   );
 
 const readAndAppend = (content, file) => {
